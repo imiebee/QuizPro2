b5d4eae468be359ce22026c6097dbe82f5744fc2
imiebee <imiebee@gmail.com>
2022-12-18 07:12:57 +0000

    Created app.js - ja for menu

---
+++ app.js
@@ -0,0 +1,6 @@
+/* Code obtained from https://alvarotrigo.com/blog/hamburger-menu-css/ */
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+  }
