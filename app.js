7d673380baff520269982d659d0c0bb1d0f4fac9
imiebee <imiebee@gmail.com>
2022-12-22 17:01:42 +0000

    Added js for to start game, loop through questions, keep score, timeout for new question

--- app.js
+++ app.js
@@ -109,3 +109,65 @@ let questions = [
     answer: 4
   },
   ];
+
+  const CORRECT_BONUS = 1;
+const MAX_QUESTIONS = 10;
+
+function startGame () {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  getNewQuestion();
+}
+
+function getNewQuestion () {
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+      window.localStorage.setItem("mostRecentScore", score);
+      return window.location.assign("/end.html");
+  }
+  questionCounter++;
+  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuestions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+}
+
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if(!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+    
+    const classToApply = 
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+    
+      if(classToApply === 'correct') {
+        incrementScore(CORRECT_BONUS);
+      }
+
+      selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+
+  });
+});
+
+incrementScore = num => {
+  score += num;
+  scoreText.innerText = score;
+};
+startGame();
+
