a16ba05fcb409aa90f173e21432559ab07526f50
imiebee <imiebee@gmail.com>
2022-12-22 17:03:48 +0000

    Fixed error on line 13 - duplicate variable removed

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 const questionCounterText = document.getElementById('questionCounter');
 const scoreText = document.getElementById('score');
-let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
