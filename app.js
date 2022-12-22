03195f27198293b4241f7f3b71841e0f5c5edb1c
imiebee <imiebee@gmail.com>
2022-12-22 16:55:52 +0000

    Added variables for score, questionCounter in JS

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ function menuOnClick() {
 /* Code based on JS course I am doing with Udemy  */
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
 
 let currentQuestion = {};
 
